98342e21a213d39fdffe5952f40a0a1eaba7ffc9
AhuraMagda <lubaszka.magda@gmail.com>
2023-11-04 10:11:55 +0100

    prettier

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,44 @@
-
 import { render as renderCategories } from "./helpers/redner-categories.helper.js";
 import { render } from "./helpers/render-tasks.helper.js";
 import { Task, Category } from "./types/types.js";
 
 const tasksContainerElement: HTMLElement = document.querySelector(".tasks");
-const categoriesUl: HTMLElement = document.querySelector(".categories")
+const categoriesUl: HTMLElement = document.querySelector(".categories");
 let selectedCategory: Category;
 
 const categories: Category[] = [
-    Category.GENERAL,
-    Category.CODING,
-    Category.IMPORTANT,
+  Category.GENERAL,
+  Category.CODING,
+  Category.IMPORTANT,
 ];
 
 const tasks: Task[] = [
-    new Task("learn TS", false, Category.IMPORTANT),
-    new Task("do qualtrics project", false, Category.CODING),
-    new Task("drink water", true),
-]
+  new Task("learn TS", false, Category.IMPORTANT),
+  new Task("do qualtrics project", false, Category.CODING),
+  new Task("drink water", true),
+];
 
 const updateSelectedCategory = (newCategory: Category) => {
-    selectedCategory = newCategory;
-}
+  selectedCategory = newCategory;
+};
 
 const addTask = (Task: Task) => {
-    tasks.push(Task)
-}
+  tasks.push(Task);
+};
 
 const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
 const taskButton: HTMLButtonElement = document.querySelector("button");
 
 taskButton.addEventListener("click", (event: Event) => {
-    event.preventDefault();
-    const newTask = new Task(taskNameInputElement.value, false, selectedCategory);
-    addTask(newTask);
-    newTask.logCreationDate("!!!")
-    render(tasks, tasksContainerElement)
-})
+  event.preventDefault();
+  const newTask = new Task(taskNameInputElement.value, false, selectedCategory);
+  addTask(newTask);
+  newTask.logCreationDate("!!!");
+  render(tasks, tasksContainerElement);
+});
 
 const taskInstance = new Task("tidy the room", true);
-addTask(taskInstance)
+addTask(taskInstance);
 
 renderCategories(categories, categoriesUl, updateSelectedCategory);
 render(tasks, tasksContainerElement);
-
--- src/helpers/redner-categories.helper.ts
+++ src/helpers/redner-categories.helper.ts
@@ -1,51 +1,55 @@
-
 import { Category } from "../types/types.js";
 
+export const render = (
+  categories: Category[],
+  categoriesUl: HTMLElement,
+  inputChangeCallback: (category: Category) => void
+) => {
+  const handleCategoryChange = (category: Category) => {
+    const button = document.querySelector("button");
+    let buttonTextColor: string;
+    switch (category) {
+      case Category.GENERAL:
+        buttonTextColor = "black";
+        break;
+      case Category.CODING:
+        buttonTextColor = "darkgoldenrod";
+        break;
+      case Category.IMPORTANT:
+        buttonTextColor = "rgb(197, 8, 8)";
+        break;
+      default:
+        const never: never = category;
+        console.log(never);
+        break;
+    }
+    button.style.color = buttonTextColor;
+  };
 
-export const render = (categories: Category[], categoriesUl: HTMLElement, inputChangeCallback: (category: Category) => void) => {
-    
-    const handleCategoryChange = (category: Category) => {
-        const button = document.querySelector("button")
-        let buttonTextColor: string;
-        switch (category) {
-            case Category.GENERAL:
-                buttonTextColor = "black";
-                break;
-            case Category.CODING:
-                buttonTextColor = "darkgoldenrod";
-                break;
-            case Category.IMPORTANT:
-                buttonTextColor = "rgb(197, 8, 8)";
-                break;
-            default:
-                const never: never = category;
-                console.log(never);
-                break;
-        }
-        button.style.color = buttonTextColor;
-    };
-    
-    categories.forEach(category => {
-        const categoryInput: HTMLInputElement = document.createElement("input");
-        categoryInput.type = "radio";
-        categoryInput.name = "category";
-        categoryInput.value = category;
-        categoryInput.id = `category-${category}`;
-        categoryInput.style.color = "red";
-        categoryInput.addEventListener("change", () => {inputChangeCallback(category); handleCategoryChange(category);});
+  categories.forEach((category) => {
+    const categoryInput: HTMLInputElement = document.createElement("input");
+    categoryInput.type = "radio";
+    categoryInput.name = "category";
+    categoryInput.value = category;
+    categoryInput.id = `category-${category}`;
+    categoryInput.style.color = "red";
+    categoryInput.addEventListener("change", () => {
+      inputChangeCallback(category);
+      handleCategoryChange(category);
+    });
 
-        const categoryLabel: HTMLLabelElement = document.createElement("label");
-        categoryLabel.setAttribute("for", `category-${category}`);
-        categoryLabel.innerHTML = category;
-        categoryLabel.style.color = category === Category.GENERAL && "black";
-        categoryLabel.style.color = category === Category.CODING && "darkgoldenrod";
-        categoryLabel.style.color = category === Category.IMPORTANT && "rgb(197, 8, 8)";
+    const categoryLabel: HTMLLabelElement = document.createElement("label");
+    categoryLabel.setAttribute("for", `category-${category}`);
+    categoryLabel.innerHTML = category;
+    categoryLabel.style.color = category === Category.GENERAL && "black";
+    categoryLabel.style.color = category === Category.CODING && "darkgoldenrod";
+    categoryLabel.style.color =
+      category === Category.IMPORTANT && "rgb(197, 8, 8)";
 
-        const categoryLi = document.createElement("li");
-        categoryLi.appendChild(categoryInput);
-        categoryLi.appendChild(categoryLabel);
+    const categoryLi = document.createElement("li");
+    categoryLi.appendChild(categoryInput);
+    categoryLi.appendChild(categoryLabel);
 
-        categoriesUl.appendChild(categoryLi);
-        
-    })
-}
+    categoriesUl.appendChild(categoryLi);
+  });
+};
